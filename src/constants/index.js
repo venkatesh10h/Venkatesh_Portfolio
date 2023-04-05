a6f7d328f83f4e1e6793908afd54b94acd65af81
venkatesh10h <129957020+venkatesh10h@users.noreply.github.com>
2023-04-05 17:51:36 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,268 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  ethicalhacking,
+  html,
+  css,
+  reactjs,
+  python,
+  linux,
+  c,
+  cplus,
+  git,
+  wordpress,
+  robotics,
+  qruzo,
+  cyberbots,
+  quatics,
+  zoho,
+  carrent,
+  jobit,
+  tripguide,
+  shopifyy,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Research Analyst",
+    icon: mobile,
+  },
+  {
+    title: "Ethical Hacker",
+    icon: backend,
+  },
+  {
+    title: "Sales Development Representative",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "Ethical Hacking",
+    icon: ethicalhacking,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "Kali Linux",
+    icon: linux,
+  },
+  {
+    name: "C Programming",
+    icon: c,
+  },
+  {
+    name: "C++",
+    icon: cplus,
+  },
+  {
+    name: "Shopify",
+    icon: shopifyy,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "Wordpress",
+    icon: wordpress,
+  },
+  {
+    name: "Robotics",
+    icon: robotics,
+  },
+];
+
+const experiences = [
+  {
+    title: "Senior Research Analyst",
+    company_name: "Cyberbots Robo Technologies",
+    icon: cyberbots,
+    iconBg: "#383E56",
+    date: "April 2022 - Present",
+    points: [
+      "Designing and delivering training programs that cover various topics related to robotics and electronics. This could include programming, circuit design, mechanical design, and other related topics.",
+      "Conducting research to stay up-to-date on the latest developments in the field of robotics and electronics, and conduct research to identify emerging trends and technologies that could be incorporated into training programs.",
+      "Developing training materials such as manuals, presentations, and videos, which can be used by participants to supplement their learning.",
+      "Responsible for evaluating the effectiveness of training programs and making recommendations for improvements.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Quatics Tech Pvt Ltd",
+    icon: quatics,
+    iconBg: "#E6DEDD",
+    date: "May 2021 - October 2022",
+    points: [
+      "Developing and maintaining web applications using React.js, Wordpress, Shopify and other related technologies.",
+      "Responsible for identifying and resolving issues with websites and applications, such as performance problems, security vulnerabilities, and compatibility issues.",
+      "Responsible for updating and maintaining WordPress websites, Shopify stores, and React applications, including adding new content, updating themes and plugins, and ensuring that websites and applications are secure and up-to-date.",
+      "Working closely with designers and other developers to ensure that websites and applications meet the client's requirements.",
+      "Providing technical support to clients and end-users, answering their questions and helping them troubleshoot any problems they encounter."
+
+    ],
+  },
+  {
+    title: "Sales Development Representative",
+    company_name: "Qruzo Pvt Ltd",
+    icon: qruzo,
+    iconBg: "#383E56",
+    date: "November 2020 - April 2021",
+    points: [
+      "Identifying and reaching out to potential customers, such as schools, universities, training providers, and other educational institutions.",
+      "Evaluating leads to determine if they are a good fit for your organization's products or services, and whether they have the budget and decision-making authority to make a purchase.",
+      "Building relationships with prospects through regular communication, such as phone calls, emails, and social media, and providing them with relevant information to help move them through the sales funnel.",
+      "Meeting or exceeding sales targets by closing deals with qualified prospects and keeping accurate records of all sales activities, including leads, contacts, and sales data.",
+    ],
+  },
+  {
+    title: "SalesForce Creator Internship",
+    company_name: "Fristine Infotech - Zoho CRM",
+    icon: zoho,
+    iconBg: "#E6DEDD",
+    date: "Jan 2020 - March 2020",
+    points: [
+      "Responsible for designing and developing Salesforce applications using the Salesforce Lightning platform.",
+      "Responsible for creating custom objects, fields, and relationships within the Salesforce platform to meet business requirements.",
+      "Creating workflows and automations within the Salesforce platform to streamline business processes and improve efficiency.",
+      "Responsible for creating reports and dashboards within Salesforce to provide insights into business performance and inform decision-making.",
+      "Work closely with business stakeholders, such as sales teams and marketing teams, to understand their requirements and develop solutions that meet their needs."
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Venkatesh's tech-savviness is impressive - always ahead of the curve in embracing emerging technologies and proficient in adapting to new tools. Their passion for technology and innovation is contagious, inspiring us to push beyond our limits. His ability to stay up-to-date with the latest trends in their field is remarkable.",
+    name: "Pavithra",
+    designation: "Senior Talent Aquisition Specialist",
+    company: "Cyberbots",
+    image: "https://media.licdn.com/dms/image/D5635AQFSMDLykC4Aow/profile-framedphoto-shrink_400_400/0/1679919009191?e=1681293600&v=beta&t=QXCUNuExVvhQ6J6gYkwfrzacArijv8yD6Iz9gk2PB2g",
+  },
+  {
+    testimonial:
+      "Venkatesh is an exceptional professional, with outstanding technical and practical skills. I have had the pleasure of working with him and can attest to his talent for finding solutions to complex problems. Venkatesh is well-respected by his colleagues for his strong work ethic and adherence to a set of rules, and he is always willing to lend a helping hand despite his quiet demeanor. I highly recommend Venkatesh to anyone looking for a skilled and reliable team player.",
+    name: "Kesavan K",
+    designation: "Business Development Executive",
+    company: "Cyberbots",
+    image: "https://media.licdn.com/dms/image/D4D35AQFhIF2jnDUIzA/profile-framedphoto-shrink_400_400/0/1679512927520?e=1681293600&v=beta&t=0fMVhN3_PFFhKqA69agcfBrMhC2cnY2rHOgyCweHMXc",
+  },
+  {
+    testimonial:
+      "",
+    name: "Rubi lekha",
+    designation: "Human Resource Manager",
+    company: "Cyberbots",
+    image: "https://media.licdn.com/dms/image/D5603AQEAn1IUaHGAHg/profile-displayphoto-shrink_400_400/0/1677168581261?e=1686182400&v=beta&t=VPFJSfUSsZKimTxGdUi1fCNyQpLHMDWPXqVGP0en3yo",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent Website",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job Portal Website",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide Website",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
